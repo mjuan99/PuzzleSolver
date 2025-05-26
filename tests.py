9a605d132c619c446d12e4c9c9fd6aa6e8e676b4
Juan Manuel Marten <juan.marten@assertia.com.ar>
2025-05-25 21:55:23 -0300

    update testing script

--- tests.py
+++ tests.py
@@ -1,101 +1,41 @@
-from read_write_helper import write_to_file, read_from_file
-# from src.puzzles.rubiks_cube_puzzle import RubiksCube
-from src.puzzles.rubiks_222_cube_puzzle import Rubiks222Cube
-from src.models.r222_cube.r222_MLP_model import Rubiks222CubeMLPModel
-from src.models.random_model import RandomModel
-from src.models.random_normal_model import RandomNormalModel
-from src.models.bfs_model import BFSModel
+from src.puzzles.rubiks_cube_puzzle import RubiksCube
 from puzzle_solver import PuzzleSolver
-# from src.models.rubiks_cube.rubiks_MLP_model import RubiksCubeMLPModel
-# from src.models.rubiks_cube.rubiks_basic_MLP_model import RubiksCubeBasicMLPModel
-# from src.models.rubiks_cube.rubiks_XGB_model import RubiksCubeXGBModel
-# from src.models.rubiks_cube.rubiks_CNN_model import RubiksCubeCNNModel
+from src.models.rubiks_cube.rubiks_MLP_model import RubiksCubeMLPModel
 
 
-# p = Rubiks222Cube()
-# s = PuzzleSolver(p, read_from_file("pkls/r222_complex_MLP_11_50000.pkl"))
-
-# states = read_from_file("pkls/r222_states_0_0.pkl")
-# states = [(s, l) for (s, l) in states if l == 11]
-
-# for c, _ in states:
-#   solution, visited_nodes, elapsed_time = s.solve(c)
-#   print(f"HMA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-  
-# exit()
-
-# for distance in [5, 7, 9, 11]:
-#   print(f"Distance: {distance}")
-#   for iteration in range(1, 6):
-#     c = p.new_puzzle(total_movements=distance)
-#     solution, visited_nodes, elapsed_time = s.solve(c)
-#     print(f"HMA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-#   solution, visited_nodes, elapsed_time = s.bfs_solve(c)
-#   print(f"BFS: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-
-
-
-# exit()
-
-
-
-p = Rubiks222Cube()
-m = read_from_file("pkls/r222_complex_MLP_0_0.pkl")
-s = PuzzleSolver(p, m)
-c = p.from_string("GGBY WWGB OYYW OGBO ORBY RWRR")
-print(p.to_string(c))
-
-solution, visited_nodes, elapsed_time = s.solve(c)
-print(f"HMA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-solution, visited_nodes, elapsed_time = PuzzleSolver(p, RandomNormalModel(11)).solve(c, max_depth=9)
-print(f"HNA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-solution, visited_nodes, elapsed_time = PuzzleSolver(p, RandomModel(11)).solve(c, max_depth=9)
-print(f"HRA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-solution, visited_nodes, elapsed_time = PuzzleSolver(p, RandomNormalModel(11)).solve(c, max_depth=11)
-print(f"HNA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-solution, visited_nodes, elapsed_time = PuzzleSolver(p, RandomModel(11)).solve(c, max_depth=11)
-print(f"HRA: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-
-solution, visited_nodes, elapsed_time = s.bfs_solve(c)
-print(f"BFS: Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s - Solution: {solution}")
-exit()
-
-
-def generate_states_and_save(max_depth, max_level_size, puzzle, states_file_path):
-  solver = PuzzleSolver(puzzle)
-  states = solver.generate_states(max_depth=max_depth, max_level_size=max_level_size)
-  write_to_file(states_file_path, states)
-
-def train_model_and_save(states, model, model_file_path):
-  solver = PuzzleSolver(None, model)
-  solver.states = states
-  solver.train_model()
-  write_to_file(model_file_path, solver.model)
-  solver.evaluate_model()
-
-def evaluate(solver, c, name):
-  solution, visited_nodes, elapsed_time = solver.solve(c)
-  print(f"{name:12} - Visited Nodes: {visited_nodes} - Elapsed Time: {elapsed_time:6.4f}s")
-
-
+# Setup Puzzle Solver to learn to solve a Rubik's 3x3x3 Cube using a MLP model as heuristic function
 rubiks_cube_puzzle = RubiksCube()
-cnn_model = RubiksCubeCNNModel()
-solver = PuzzleSolver(rubiks_cube_puzzle, cnn_model)
+rubiks_cube_MLP_model = RubiksCubeMLPModel()
+solver = PuzzleSolver(rubiks_cube_puzzle, rubiks_cube_MLP_model)
 
-solver.states = read_from_file('pkls/rubik_states_15_50000.pkl')
 
+# Generate training states, up to 15 moves away from the final state and up to 10.000 states for each level
+solver.generate_states(max_depth=15, max_level_size=10000)
+# Train the model on the generated states dataset
 solver.train_model()
-solver.evaluate_model()
+# Display some model evaluation if needed
+#solver.evaluate_model()
 
-write_to_file('pkls/rubik_CNN.pkl', solver.model)
 
-solver2 = PuzzleSolver(rubiks_cube_puzzle, read_from_file('pkls/rubik_complex_MLP_15_50000.pkl'))
-c = rubiks_cube_puzzle.new_puzzle(total_movements=8)
+# Create a new unsolved Rubik's Cube (up to) 5 movements away from the final state
+c = rubiks_cube_puzzle.new_puzzle(total_movements=5)
+print("Puzzle to solve:")
+print(rubiks_cube_puzzle.to_string(c))
 
-evaluate(solver, c, "CNN")
-evaluate(solver2, c, "MLP")
+# Solve it with the Puzzle Solver A* algorithm. Should be really quick for a state so close to the final state
+solution, visited_nodes, elapsed_time = solver.solve(c)
+
+print("Heuristic Model Puzzle Solver:\n")
+print(f"Solution found: {solution}")
+print(f"Visited Nodes:  {visited_nodes}")
+print(f"Elapsed Time:   {elapsed_time:.4f}s")
+
+
+# To compare, solve it with a BFS algorithm (brute force). Should take some time even though the state is close to the final state
+print("\n\nCompare with BFS:\n")
+
+solution, visited_nodes, elapsed_time = solver.bfs_solve(c)
+
+print(f"Solution found: {solution}")
+print(f"Visited Nodes:  {visited_nodes}")
+print(f"Elapsed Time:   {elapsed_time:.4f}s")
